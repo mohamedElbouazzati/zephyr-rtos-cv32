70d8aa6583206a2f1f5b100f437c75fa687dbc67
Mohamed EL BOUAZZATI <elbouazzati.mohamed.2e@gmail.com>
2024-05-24 08:47:54 +0200

    comment debug code

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,27 +7,27 @@
 #include <stdio.h>
 #include <soc.h>
 
-void printBinary(int num) {
-    for (int i = 31; i >= 0; i--) {
-        printf("%d", (num >> i) & 1);
-        if (i % 4 == 0)
-            printf(" ");
-    }
-    printf("\n");
-}
+// void printBinary(int num) {
+//     for (int i = 31; i >= 0; i--) {
+//         printf("%d", (num >> i) & 1);
+//         if (i % 4 == 0)
+//             printf(" ");
+//     }
+//     printf("\n");
+// }
 
 int main(void)
 {
-	uint32_t ev_pending = 0x82003010;
-	uint32_t ev_enable = 0x82003014;
+	// uint32_t ev_pending = 0x82003010;
+	// uint32_t ev_enable = 0x82003014;
 	printf("Hello World! %s\n", CONFIG_BOARD);
 	// __asm__ volatile ("csrw %0, %1" :: "i"(0x344), "r"(0));
 	// litex_write32(1, 0x82001800);
 
-		uint32_t mtvec;
+		// uint32_t mtvec;
 
-	__asm__ volatile ("csrr %0, %1" : "=r"(mtvec) : "i"(0x305));
-	printBinary(mtvec);
+	// __asm__ volatile ("csrr %0, %1" : "=r"(mtvec) : "i"(0x305));
+	// printBinary(mtvec);
 	// __asm__ volatile ("csrw %0, %1" :: "i"(0x344), "r"((mask & ~(1<<0)<<16)));
 
 	// litex_write32(ev_pending,litex_read32(ev_pending));
